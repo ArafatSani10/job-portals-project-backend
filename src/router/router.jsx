03c805d1e35329e12d9e38ba61cbb81bfb640f9d
ArafatSani10 <mdarafatrahmansani17@gmail.com>
2024-12-11 20:52:39 +0600

    first commit

---
+++ src/router/router.jsx
@@ -0,0 +1,16 @@
+import {
+    createBrowserRouter,
+
+  } from "react-router-dom";
+
+
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Hello world!</div>,
+    },
+  ]);
+  
+
+  export default router;
